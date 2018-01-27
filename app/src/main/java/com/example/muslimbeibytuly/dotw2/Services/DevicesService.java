cbc3c687dbebddf71fc7c298a3577f8236dc1dc3
muslimbeibytuly <muslimbeibytuly@gmail.com>
2018-01-27 17:29:51 +0600

    clear to connect

---
+++ app/src/main/java/com/example/muslimbeibytuly/dotw2/Services/DevicesService.java
@@ -0,0 +1,38 @@
+package com.example.muslimbeibytuly.dotw2.Services;
+
+import android.net.wifi.p2p.WifiP2pDevice;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by muslimbeibytuly on 1/27/18.
+ */
+
+public class DevicesService {
+    private static DevicesService instance = null;
+    public static DevicesService getInstance() {
+        if (instance == null) {
+            instance = new DevicesService();
+        }
+        return instance;
+    }
+    private ArrayList<WifiP2pDevice> p2pDevices;
+
+    public ArrayList<WifiP2pDevice> getP2pDevices() {
+        return p2pDevices;
+    }
+
+    public void setP2pDevices(Collection<WifiP2pDevice> p2pDevices) {
+
+        this.p2pDevices = new ArrayList<>(p2pDevices);
+    }
+
+    public ArrayList<String> getP2pDevicesNames() {
+        ArrayList<String> devices = new ArrayList<>();
+        for (WifiP2pDevice device : p2pDevices) {
+            devices.add(device.deviceName);
+        }
+        return devices;
+    }
+}
